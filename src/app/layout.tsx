df1e2e08ee8e80f8b632c52473337fe93bdc4ccc
Cyber Research <researchcyber292@gmail.com>
2026-01-24 14:27:16 +0000

    remove this option completely. (_for element <Header>_)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import { Toaster } from "@/components/ui/toaster";
-import { Header } from '@/components/layout/header';
 import { Footer } from '@/components/layout/footer';
 import './globals.css';
 
@@ -25,7 +24,6 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         <div className="flex min-h-screen flex-col">
-          <Header />
           <main className="flex-1">{children}</main>
           <Footer />
         </div>
