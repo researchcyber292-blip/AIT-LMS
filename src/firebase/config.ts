79d323748ea60b374dbbd1a7997f45410e310bf0
Cyber Research <researchcyber292@gmail.com>
2026-01-25 07:51:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-996477947-ebd2c",
+  "appId": "1:657452916833:web:add5a283f90c46e60dca20",
+  "apiKey": "AIzaSyDnfU0c_G31OTpH3dK1pqHRddfmBg3M1yQ",
+  "authDomain": "studio-996477947-ebd2c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "657452916833"
+};
