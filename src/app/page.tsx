466d837bd66ed057f406df5ee57ee20b83593f44
Cyber Research <researchcyber292@gmail.com>
2026-02-06 11:13:24 +0000

    NO  YOU HAVE MAKED THE LEFT IMAGE UP I WANT THE IMAGE SHOULD COME IN DOW

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -100,7 +100,7 @@ export default function Home() {
               </div>
                <div className="relative h-96 md:h-full w-full flex items-center justify-between">
                       {/* Left Character (CEO - Answering) */}
-                      <div className="relative transform translate-y-8">
+                      <div className="relative transform translate-y-2">
                           {/* Dashed Circle */}
                           <div className="absolute -inset-4 rounded-full border-2 border-dashed border-primary/30 animate-spin-slow"></div>
                           {/* Floating Dots */}
