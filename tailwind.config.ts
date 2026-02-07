2c036629fbfdd85dae90057cf62d765bd4be1d68
Cyber Research <researchcyber292@gmail.com>
2026-02-07 14:32:01 +0000

    AND WHY IN THE HOME PAGE AT HERO SECTION THE OUTER CIRCULAR LAYER NOW NO

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -105,6 +105,7 @@ export default {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'border-spin': 'border-spin 20s linear infinite',
+        'spin-slow': 'spin 20s linear infinite',
       },
     },
   },
