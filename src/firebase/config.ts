ba2a3776acf17f1f3ba148e01c8372971dd40f7c
Cyber Research <researchcyber292@gmail.com>
2026-02-10 13:45:13 +0000

    WHY IT COUNTAINING API KEY OPENLY : src/firebase/config.ts export const

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-996477947-ebd2c",
-  "appId": "1:657452916833:web:add5a283f90c46e60dca20",
-  "apiKey": "AIzaSyDnfU0c_G31OTpH3dK1pqHRddfmBg3M1yQ",
-  "authDomain": "studio-996477947-ebd2c.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "657452916833"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
